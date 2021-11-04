e9486010db6d3b7cab08c86f40b8476e34e28975
Noel Semaan <64365978+NoelSemaan@users.noreply.github.com>
2021-11-04 21:27:35 +0200

    Learning the variables

---
+++ Variables.py
@@ -0,0 +1,12 @@
+"""Here between the quotes is written the documentation where i will explain the work in progress:
+python unlike other languages (ex:java) does not need the variable to be declared by a type
+the basic types are: 
+int() ===> non decimal numbers like 1 5 9 10000 ... 
+float() ===> decimal numbers like 2.3 7.1234 0.00045 ...
+string() ===> everything considered to be a text that is between '' like 'Hi'  'Hello' 'John Doe' '25'
+boolean() ===> two returns are possible either True or False
+"""
+x = 5 #This is an int
+y = 3.2 # this is a float
+z = 'Hello'# this is a String
+b = True
