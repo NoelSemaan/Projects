64971d8be209daae123913320205f3016ae37b5b
Noel Semaan <64365978+NoelSemaan@users.noreply.github.com>
2021-11-06 09:57:46 +0200

    learning the comparison operators

---
+++ 4-Comparison.py
@@ -0,0 +1,13 @@
+"""In this tutorial we will learn about comparing values of variables and the return value will be a boolean(True/False)"""
+a = 1
+b = 2
+# to compare if two values are equal we use the double = sign -----> ==
+a == b # the result will be False for they are different in values
+a != b # the ! is used for negation(as in not) so here we are asking if a is not equal to b and the result is True
+a > b # is a greater than b the result is False
+a < b # is a less than b the result is True
+a >= b # is a greater or equal to b the result is False
+a <= b # is a less than or equal to b the result is True
+
+#example
+print(a>=b) #False
